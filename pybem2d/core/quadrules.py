3ae491c9d2a185972ebbe1ba94fda723257078d2
Timo Betcke <timo.betcke@gmail.com>
2011-02-06 11:14:45 +0000

    Quadrature over singular element implemented

--- pybem2d/core/quadrules.py
+++ pybem2d/core/quadrules.py
@@ -1,6 +1,7 @@
 import scipy.special
 import numpy
 
+from pylab import plot,show
 
 
 def gauss(n):
@@ -9,12 +10,19 @@ def gauss(n):
     w=.5*w
     return x,w
 
+def tensorquad(x,wx,y,wy):
+    """Combine two quadrules in the x and y direction into a 2D tensor rule"""
+    
+    nx=len(x)
+    ny=len(y)
+    m=numpy.mgrid[0:nx,0:ny].reshape(2,-1)
+    x2=numpy.vstack((x[m[0]],y[m[1]]))
+    w2=wx[m[0]]*wy[m[1]]
+    return x2,w2
+
 def gauss2d(n):
     x,w=gauss(n)
-    m=numpy.mgrid[0:n,0:n].reshape(2,-1)
-    w2=w[m[0]]*w[m[1]]
-    x2=numpy.vstack((x[m[0]],x[m[1]]))
-    return x2,w2
+    return tensorquad(x,w,x,w)
 
 def shift2d(x,w,a,b):
     """Shift a 2d quadrature rule to the box defined by the tuples a and b"""
@@ -26,12 +34,44 @@ def shift2d(x,w,a,b):
 class GaussQuadrature:
     """Container class containing the needed 2D quadrature rules"""
 
-    def __init__():
-        pass
+    def __init__(self,n,recDepth,sigma):
+        self.x,self.w=gauss(n)
+        self.x2,self.w2=gauss2d(n)
+        self.recDepth=recDepth
+        self.sigma=sigma
+        
+        self.adapted1D()
+        self.singElement()
+
+    def adapted1D(self):
+
+        self.x_adapted1D=numpy.array([])
+        self.w_adapted1D=numpy.array([])
+        a,b=self.sigma,1
+        for i in range(self.recDepth+1):
+            self.x_adapted1D=numpy.hstack([self.x_adapted1D,a+(b-a)*self.x])
+            self.w_adapted1D=numpy.hstack([self.w_adapted1D,(b-a)*self.w])
+            a*=self.sigma
+            b*=self.sigma
+
+    def singElement(self):
+
+        x,w=tensorquad(self.x_adapted1D,self.w_adapted1D,self.x_adapted1D,self.w_adapted1D)
+        x1=numpy.array([x[0],1-x[1]])
+        x1[1,:]*=x1[0,:]
+        w1=w*x1[0,:]
+        x2=numpy.array([1-x[0],x[1]])
+        x2[1,:]=x2[0,:]+x2[1,:]*(1-x2[0,:])
+        w2=w*(1-x1[0])
+        self.x_singElement=numpy.hstack([x1,x2])
+        self.w_singElement=numpy.hstack([w1,w2])
 
 
-if __name__ == "__main__":
-    print gauss(2)
-    x,w=gauss2d(2)
-    print shift2d(x,w,(1,1),(4,3))
+if  __name__ == "__main__":
+    #print gauss(2)
+    x,w=gauss(10)
+    x2,w2=shift2d(x,w,(0,0),(1,1))
+    g=GaussQuadrature(10,2,0.15)
+    plot(g.x_singElement[0,:],g.x_singElement[1,:],'r.')
+    show()
 
