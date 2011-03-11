82796ba0e3ec6a26eacb4e8738872aafde1e83e1
Timo Betcke <timo.betcke@gmail.com>
2011-03-11 22:58:04 +0000

    Objects can now be imported directly from pybem2d

---
+++ pybem2d/__init__.py
@@ -0,0 +1,14 @@
+from core.segments import Segment,Line,Arc,Ellipse
+from core.mesh import Domain,Mesh
+from core.bases import ElementToBasis, MeshToBasis, NodalLin, Legendre
+from core.kernels import (LogSingleLayer, LogDoubleLayer, LogConjDoubleLayer,
+AcousticSingleLayer, AcousticDoubleLayer, AcousticConjDoubleLayer,
+AcousticCombined, Identity)
+from core.assembly import Assembly
+from core.quadrules import GaussQuadrature
+from core.evaluation import Evaluator
+from core.visualization import Visualizer
+
+
+
+
