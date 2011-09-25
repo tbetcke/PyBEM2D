0c281c952e246d1ce76f76c59839b896e04a8fa6
Timo Betcke <timo.betcke@gmail.com>
2011-09-25 18:54:33 +0100

    Update to module exports

--- pybem2d/__init__.py
+++ pybem2d/__init__.py
@@ -7,7 +7,7 @@ AcousticCombined, Identity)
 from core.assembly import Assembly
 from core.quadrules import GaussQuadrature
 from core.evaluation import Evaluator, evalDensity
-from core.visualization import Visualizer
+from core.visualization import Visualizer, plotDensity
 
 
 
